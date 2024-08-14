ba217e8d2aa081aa32a9f4d41898f9fb7851bf4e
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-14 22:28:49 +0500

    Allowing Api Path for registeration

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import express from 'express'
 import { Port } from './Config.js'
+import UserRouter from './DB/RegisterandSignInUser.js'
 
 const App = express()
 
 App.use(express.json())
+
+App.use('/api/Users', UserRouter)
 
 App.listen(Port, () => {
   console.log(`RUNNING ON ${Port}`)
