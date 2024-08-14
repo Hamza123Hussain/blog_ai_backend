f0e7a3fd72c8ea793c446d53f1cb1b4a7c3c1cf4
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-15 03:27:28 +0500

    Allowing comment Api

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import UserRouter from './DB/RegisterandSignInUser.js'
 import UserPosts from './DB/GettingPostForUser.js'
 import Create_Get_Router from './DB/GettingandCreatingPost.js'
 import Updated_Delete_Router from './DB/UpdateandDeleteDoc.js'
+import CommentRouter from './DB/PostingComment.js'
 
 const App = express()
 
@@ -13,6 +14,7 @@ App.use('/api/Users', UserRouter)
 App.use('/api/Posts', Create_Get_Router)
 App.use('/api/User', UserPosts)
 App.use('/api/Update&Delete', Updated_Delete_Router)
+App.use('/api/Comments', CommentRouter)
 
 App.listen(Port, () => {
   console.log(`RUNNING ON ${Port}`)
