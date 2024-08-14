38a17afd4d80e46b329b6aced594cbb1ff18571a
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-14 22:28:10 +0500

    exporting auth and db for their respective functions

--- FireBaseConfig.js
+++ FireBaseConfig.js
@@ -8,6 +8,8 @@ import {
   projectId,
   storageBucket,
 } from './Config.js'
+import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -23,3 +25,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
+// Initialize Firestore and Authentication
+export const db = getFirestore(app)
+export const auth = getAuth(app)
