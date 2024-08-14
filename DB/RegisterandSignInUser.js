4ff5fc2e408b60d4da5854f7c0e41d48006c7803
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-14 22:28:35 +0500

    User Registration Successful

---
+++ DB/RegisterandSignInUser.js
@@ -0,0 +1,65 @@
+import { Router } from 'express'
+import { v4 as uuidv4 } from 'uuid'
+import { createUserWithEmailAndPassword } from 'firebase/auth'
+import { doc, setDoc, arrayUnion } from 'firebase/firestore'
+import { auth, db } from '../FireBaseConfig.js'
+
+const UserRouter = Router()
+
+UserRouter.post('/', async (req, res) => {
+  const randomId = uuidv4()
+
+  try {
+    const { email, password, name } = req.body
+
+    // Create a new user with Firebase Authentication
+    const userCredential = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    )
+
+    if (userCredential.user) {
+      const userId = userCredential.user.uid
+
+      // Add user details to Firestore
+      await setDoc(doc(db, 'Users', userId), {
+        Name: name,
+        email: email,
+        userID: userId,
+        CreatedAt: Date.now(),
+      })
+
+      // Initialize the user's post in Firestore
+      await setDoc(doc(db, 'Posts', email), {
+        PostDetails: arrayUnion({
+          PostID: randomId,
+          CreatedBy: email,
+          Text: '',
+          ImageURL: '',
+          comments: 0,
+          likes: 0,
+        }),
+      })
+
+      // Send success response with user and post information
+      res.status(201).json({
+        status: 'success',
+        message: 'User and post created successfully',
+        user: {
+          userID: userId,
+          email: email,
+          name: name,
+        },
+      })
+    } else {
+      res.status(400).json({ status: 'error', message: 'User creation failed' })
+    }
+  } catch (error) {
+    // Handle errors and send error response
+    console.error('Error creating user or post:', error)
+    res.status(500).json({ status: 'error', message: 'Internal server error' })
+  }
+})
+
+export default UserRouter
