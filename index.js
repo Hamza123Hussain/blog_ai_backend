896083fe6c5879d57c290febb1a61b7df02f6a48
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-15 02:46:28 +0500

    allowing 2 api paths. the first belongs to the user router that will handle all user

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 import express from 'express'
 import { Port } from './Config.js'
 import UserRouter from './DB/RegisterandSignInUser.js'
-import Create_Update_Router from './DB/PostCreateandUpdate.js'
+import UserPosts from './DB/GettingPostForUser.js'
+import Create_Get_Router from './DB/GettingandCreatingPost.js'
 
 const App = express()
 
 App.use(express.json())
 
 App.use('/api/Users', UserRouter)
-App.use('/api/Posts', Create_Update_Router)
+App.use('/api/Posts', Create_Get_Router)
+App.use('/api/User', UserPosts)
 
 App.listen(Port, () => {
   console.log(`RUNNING ON ${Port}`)
