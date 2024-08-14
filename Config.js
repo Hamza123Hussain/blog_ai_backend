dcbf07f197f72c7076be41e3feead602b7ec9cfd
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-14 21:49:43 +0500

    Add  all the secrets from env in the config file

--- Config.js
+++ Config.js
@@ -1,2 +1,18 @@
 import 'dotenv/config'
-export const Port = process.env.Port
+const Port = process.env.Port
+const apiKey = process.env.REACT_APP_FIREBASE_API_KEY
+const authDomain = process.env.REACT_APP_FIREBASE_AUTH_DOMAIN
+const projectId = process.env.REACT_APP_FIREBASE_PROJECT_ID
+const storageBucket = process.env.REACT_APP_FIREBASE_STORAGE_BUCKET
+const messagingSenderId = process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID
+const appId = process.env.REACT_APP_FIREBASE_APP_ID
+
+export {
+  Port,
+  apiKey,
+  appId,
+  authDomain,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+}
