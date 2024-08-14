ac0a7c9911b0c6e66e978973573eb2c933507751
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-14 23:14:57 +0500

    Allowing Api Path for posts

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 import express from 'express'
 import { Port } from './Config.js'
 import UserRouter from './DB/RegisterandSignInUser.js'
+import Create_Update_Router from './DB/PostCreateandUpdate.js'
 
 const App = express()
 
 App.use(express.json())
 
 App.use('/api/Users', UserRouter)
+App.use('/api/Posts', Create_Update_Router)
 
 App.listen(Port, () => {
   console.log(`RUNNING ON ${Port}`)
