0a8e685eba15843f53a1b73e35f87df0a45f08ec
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-15 21:24:31 +0500

    showing alert that shows error when incorrect email or password

--- DB/RegisterandSignInUser.js
+++ DB/RegisterandSignInUser.js
@@ -65,7 +65,7 @@ UserRouter.post('/Login', async (req, res) => {
       // Retrieve user data from Firestore
       const userDoc = await getDoc(doc(db, 'Users', userCredential.user.uid))
 
-      if (userDoc) {
+      if (userDoc.exists()) {
         // Send user data as JSON response
         res.status(200).json(userDoc.data())
       } else {
@@ -75,7 +75,17 @@ UserRouter.post('/Login', async (req, res) => {
       res.status(400).json({ error: 'User not registered' })
     }
   } catch (error) {
-    res.status(500).json({ error: `Server error: ${error.message}` })
+    if (
+      error.code === 'auth/wrong-password' ||
+      error.code === 'auth/user-not-found'
+    ) {
+      // Handle wrong password or user not found errors
+      res.status(500).json({ error: 'Invalid email or password' })
+    } else {
+      // Handle other errors
+      console.error('Error during login:', error)
+      res.status(401).json({ error: `Server error: ${error.message}` })
+    }
   }
 })
 
