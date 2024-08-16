eb608422f3d6e19172b84b73e60bcdf23cb46f67
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-17 01:57:18 +0500

    ALLOWING API PATH

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import Updated_Delete_Router from './DB/UpdateandDeleteDoc.js'
 import CommentRouter from './DB/PostingComment.js'
 import cors from 'cors'
 import ResetPass from './DB/PaswordReset.js'
+import AI_Router from './DB/CreatingAndUpdateWithAI.js'
 const App = express()
 
 App.use(express.json())
@@ -17,7 +18,7 @@ App.use('/api/User', UserPosts)
 App.use('/api/Update&Delete', Updated_Delete_Router)
 App.use('/api/Comments', CommentRouter)
 App.use('/api/ResetPass', ResetPass)
-
+App.use('/api/AI', AI_Router)
 App.listen(Port, () => {
   console.log(`RUNNING ON ${Port}`)
 })
