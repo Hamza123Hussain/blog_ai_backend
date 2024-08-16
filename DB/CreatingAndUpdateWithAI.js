e77a95ef0298220a7f038fdd0bd89b6ff2802f2e
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-17 02:15:27 +0500

    just sending data back from the api and not storing it directly in firebase

--- DB/CreatingAndUpdateWithAI.js
+++ DB/CreatingAndUpdateWithAI.js
@@ -1,15 +1,11 @@
 import express from 'express'
-import { doc, setDoc } from 'firebase/firestore'
-import { db } from '../FireBaseConfig.js'
 import { chatSessions } from '../GemniConfig.js'
-import { v4 as uuidv4 } from 'uuid'
 
 const AI_Router = express.Router()
 
 AI_Router.post('/', async (req, res) => {
-  const randomId = uuidv4()
   try {
-    const { text, Name, title, email } = req.body
+    const { text, title } = req.body
 
     // Create the prompt for generating the blog title
     const BlogTitlePrompt = `Read the ${title} for the description that I have given for the blog. Just give me the Title in plain string and do not include any symbols, just alphanumeric, that you think is the best for the Blog and no other text, just one single title.`
@@ -29,17 +25,6 @@ AI_Router.post('/', async (req, res) => {
 
     // Check if the generated description is valid
     if (BlogDescriptionAI) {
-      // Save the blog post to Firestore
-      await setDoc(doc(db, 'Posts', randomId), {
-        Title: BlogTitleAi,
-        CreatedBy: email,
-        UserName: Name,
-        PostID: randomId,
-        Text: BlogDescriptionAI,
-        comments: [],
-        CreatedAt: Date.now(), // likes: []
-      })
-
       // Respond with the generated blog title and description
       res.status(200).json({
         Title: BlogTitleAi,
