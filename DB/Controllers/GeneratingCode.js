8ab461f215469e0de1316303501ee9336332e299
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-18 04:47:05 +0500

    adding the boiler code that is used to run gemni successfully

--- DB/Controllers/GeneratingCode.js
+++ DB/Controllers/GeneratingCode.js
@@ -5,6 +5,9 @@ import { db } from '../../Firebase.js'
 export const CodeController = async (_, res) => {
   const RandomID = uuid()
   try {
+    // // Generate the blog title using Gemini AI
+    // const Gemni_Response = await chatSessions.sendMessage(BlogTitlePrompt)
+    // const BlogTitleAi = Gemni_Response.response.text()
     await setDoc(doc(db, 'CodeGenerator', RandomID), { message: '' })
     res.status(200).json(true)
   } catch (error) {
--- Firebase.js
+++ Firebase.js
@@ -1,24 +1,23 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app'
+import { getAnalytics } from 'firebase/analytics'
+import { getFirestore } from 'firebase/firestore'
 import {
   apiKey,
   appId,
   authDomain,
   messagingSenderId,
-  projectId,
   storageBucket,
 } from './Config.js'
-import { getAuth } from 'firebase/auth'
-import { getStorage } from 'firebase/storage'
-import { getFirestore } from 'firebase/firestore'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: apiKey,
   authDomain: authDomain,
-  projectId: projectId,
+  projectId: 'chatgpt-clone-8a3be',
   storageBucket: storageBucket,
   messagingSenderId: messagingSenderId,
   appId: appId,
@@ -26,7 +25,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
-// Initialize Firestore and Authentication
 export const db = getFirestore(app)
-export const auth = getAuth(app)
-export const Storage = getStorage(app)
