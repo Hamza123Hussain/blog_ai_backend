a3862026d05e531b50b97918ac15cc9702b7986d
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-17 18:06:17 +0500

    Added storage in the firebase config

--- FireBaseConfig.js
+++ FireBaseConfig.js
@@ -9,6 +9,7 @@ import {
   storageBucket,
 } from './Config.js'
 import { getAuth } from 'firebase/auth'
+import { getStorage } from 'firebase/storage'
 import { getFirestore } from 'firebase/firestore'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -28,3 +29,4 @@ const app = initializeApp(firebaseConfig)
 // Initialize Firestore and Authentication
 export const db = getFirestore(app)
 export const auth = getAuth(app)
+export const Storage = getStorage(app)
