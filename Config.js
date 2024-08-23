4b9bb935236f8b33ae72b0f6d14a29dede5374a9
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-23 19:18:16 +0500

    importing projectID for firebase config

--- Config.js
+++ Config.js
@@ -3,6 +3,7 @@ const Port = process.env.Port
 const apiKey = process.env.REACT_APP_FIREBASE_API_KEY
 const authDomain = process.env.REACT_APP_FIREBASE_AUTH_DOMAIN
 const storageBucket = process.env.REACT_APP_FIREBASE_STORAGE_BUCKET
+const projectId = process.env.REACT_APP_FIREBASE_PROJECT_ID
 const messagingSenderId = process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID
 const appId = process.env.REACT_APP_FIREBASE_APP_ID
 const Gemni_Api = process.env.Google_Gemni_APi
@@ -15,4 +16,5 @@ export {
   storageBucket,
   messagingSenderId,
   Gemni_Api,
+  projectId,
 }
